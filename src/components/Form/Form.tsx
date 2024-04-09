3a72817eb1677fa3e37bd1647e315a814624a61b
Layla Rodas <rodas.layla@gmail.com>
2024-04-09 14:31:45 +0200

    types -- alertcomponent - form validations

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,61 @@
+import { useState, ChangeEvent,FormEvent } from "react"
+import type { SearchType } from "../../types"
 import { countries } from "../../data/countries"
 import styles from './Form.module.css'
+import { Alert } from "../Alert/Alert"
+
+
 
 
 export const Form = () => {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const [alert, setAlert] = useState('')
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if(Object.values(search).includes('')) {
+            setAlert('All fields are required')
+            return
+        }
+    }
   return (
-    <form className={styles.form}>
+    <form 
+        className={styles.form}
+        onSubmit={handleSubmit}
+    >
+        {alert && <Alert>{alert}</Alert>}
         <div className={styles.field}>
             <label htmlFor="city">City: </label>
-            <input type="text" id="city" name="city" placeholder="City"/>
+            <input 
+                type="text" 
+                id="city" 
+                name="city" 
+                placeholder="City"
+                value={search.city}
+                onChange={handleChange}
+            />
         </div>
         <div className={styles.field}>
-            <label htmlFor="city">Country: </label>
-            <select>
+            <label htmlFor="country">Country: </label>
+            <select
+                id="country"
+                value={search.country}
+                name="country"
+                onChange={handleChange}
+            >
                 <option className={styles.options} value=""> -- Select a Country -- </option>
                 {
                     countries.map(country => (
